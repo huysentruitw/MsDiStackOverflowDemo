7665b8f58f080980382c70080ffa2f9277351789
Wouter Huysentruit <wouter_huysentruit@hotmail.com>
2021-12-01 19:41:44 +0100

    Add ASP.NET Core API sample

---
+++ AspNetCoreApi/DependencyInjection/ServiceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using AspNetCoreApi.Services;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+
+namespace AspNetCoreApi.DependencyInjection
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void AddServices(this IServiceCollection services)
+        {
+            foreach (Type serviceType in typeof(ServiceCollectionExtensions).Assembly.GetTypes()
+                .Where(type => !type.IsAbstract && typeof(IService).IsAssignableFrom(type)))
+            {
+                services.AddTransient(serviceType);
+            }
+        }
+    }
+}
